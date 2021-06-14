0ee2bb0b95efdbf1cbe89d670d961d4e24bf157e
Guilherme Vieira <guims02@hotmail.com>
2021-06-14 18:47:04 -0300

    Base do app

---
+++ src/main/java/gui/vieira/mecurso/MecursoApplication.java
@@ -0,0 +1,13 @@
+package gui.vieira.mecurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gui/vieira/mecurso/MecursoApplicationTests.java
@@ -0,0 +1,13 @@
+package gui.vieira.mecurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
