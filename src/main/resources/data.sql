3f52893a7fc4ca740e6096491473dd50de989c87
Guilherme Vieira <guims02@hotmail.com>
2021-06-14 21:48:09 -0300

    banco de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,2 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  Note Moraes
- * Created: 14/06/2021
- */
+INSERT INTO CURSO(nome,descricao,ementa,valor) VALUES('John','Aluno', 'Emen',24.5)
 
